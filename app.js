a2b2dc59eaba3168b50362c0b2d7d2b2007e40d4
Sowmya-Ramesh-7 <Soumyaramesh2002@gmail.com>
2023-03-12 17:53:38 +0530

    myapp

---
+++ app.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const app=express();
+app.get('/', function(req,res){
+    res.send('hello,world!\n');
+
+});
+app.listen(8080,function(){
+    console.log('Example app listening on port 8080 ')
+});
+module.exports=app;
